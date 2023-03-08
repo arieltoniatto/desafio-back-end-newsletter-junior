ee18dde5845fd31f86cd12fcc0d3a42389e3ca6b
arieltoniatto <ariel.toniatto@gmail.com>
2023-03-08 20:20:30 -0300

    remove comentários desnecessários

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const errorMiddleware = require('./middleware/error');
 const routesUser = require('./routes/user.router');
-// ..
 
 const app = express();
 
 app.use(express.json());
 
-// ...
 app.use('/user', routesUser);
 
 
--- src/utils/JWT.js
+++ src/utils/JWT.js
@@ -7,26 +7,6 @@ const generateToken = (payload) => {
     return token;
 };
 
-const authTokenValidation = async (token) => {
-    // if (!token) {
-    //     const e = new Error('Token not found');
-    //     e.status = 401;
-    //     throw e;
-    // }
-
-    try {
-        const instrospection = jwt.verify(token, TOKEN_SECRET);
-        return instrospection;
-    } catch (e) {
-        console.log('Err', e);
-        const err = { status: 401, message: 'Expired or invalid token' };
-        // new Error('Expired or invalid token');
-        // err.status = 401;
-        return err;
-    }
-};
-
 module.exports = {
     generateToken,
-    authTokenValidation,
 };
